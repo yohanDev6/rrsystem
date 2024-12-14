6377c42fd43b1908e409846375c6f199a875b0ae
yohanDev6 <yohan.dev6@gmail.com>
2024-12-14 16:31:43 -0300

    Raiz do backend criada

---
+++ backend/rrsystem/src/main/java/br/com/yohandevmeia/rrsystem/RrsystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.yohandevmeia.rrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrsystemApplication.class, args);
+	}
+
+}
---
+++ backend/rrsystem/src/test/java/br/com/yohandevmeia/rrsystem/RrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yohandevmeia.rrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
