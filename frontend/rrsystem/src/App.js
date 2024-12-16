57f0f7fc4584dd7514caf33212e6176bb10dcaa4
Yohan <yohan.dev6@gmail.com>
2024-12-16 18:53:00 -0300

    Configurações, organizações e arquivos iniciais do frontend

--- frontend/rrsystem/src/App.js
+++ frontend/rrsystem/src/App.js
@@ -1,13 +1,12 @@
-
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello World!</h1>
+                <p>Today is Monday</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
